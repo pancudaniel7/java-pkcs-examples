9a986b6d4845dce7c2bd9407fabdfb6fbf752111
pancudaniel7 <pancudaniel7@gmail.com>
2022-12-10 21:02:02 +0200

    RSA modifications

--- src/main/java/com/learning/RSASigning.java
+++ src/main/java/com/learning/RSASigning.java
@@ -1,21 +1,25 @@
 package com.learning;
 
-import com.learning.config.HSMConfigProvider;
 import com.learning.config.SoftHSMConfigProvider;
+import com.learning.provider.PKCS11SunSecurityProvider;
+import com.learning.provider.SecurityProvider;
 
-import java.security.KeyStore;
-import java.security.KeyStoreException;
+import java.io.IOException;
+import java.security.Provider;
 
 public class RSASigning {
 
-    private static HSMConfigProvider hsmConfigProvider;
+    private static SecurityProvider securityProvider;
 
     static {
-        hsmConfigProvider = new SoftHSMConfigProvider();
+        String configFilePath = "/tmp/softhsm.cfg";
+        String hsmSharedLibFilePath = "/opt/homebrew/Cellar/softhsm/2.6.1/lib/softhsm/libsofthsm2.so";
+
+        securityProvider = new PKCS11SunSecurityProvider(new SoftHSMConfigProvider(), configFilePath, hsmSharedLibFilePath, "1632003794");
     }
 
-    public static void main(String[] args) throws KeyStoreException {
-        KeyStore keyStore = KeyStore.getInstance("PKCS12");
-
+    public static void main(String[] args) throws IOException {
+        Provider pkcs11Provider = securityProvider.getInstance();
+        
     }
 }
