e085a5d60e1bc3debd0ff111d362af54dbac1a60
pancudaniel7 <pancudaniel7@gmail.com>
2022-12-13 16:15:55 +0200

    RSASinging check hsm key before creating new one

--- src/main/java/com/examples/RSASigning.java
+++ src/main/java/com/examples/RSASigning.java
@@ -4,16 +4,15 @@ import com.examples.config.SoftHSMConfigProvider;
 import com.examples.provider.PKCS11SunSecurityProvider;
 import com.examples.provider.SecurityProvider;
 import org.bouncycastle.jce.X509Principal;
-import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.x509.X509V3CertificateGenerator;
 
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.security.spec.InvalidKeySpecException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -29,23 +28,11 @@ public class RSASigning {
         String configFilePath = "/tmp/softhsm.cfg";
         String hsmSharedLibFilePath = "/opt/homebrew/Cellar/softhsm/2.6.1/lib/softhsm/libsofthsm2.so";
 
-        String softhsmSlotNumber = "2133921966";
+        String softhsmSlotNumber = "1372486820";
         securityProvider = new PKCS11SunSecurityProvider(new SoftHSMConfigProvider(), configFilePath, hsmSharedLibFilePath, softhsmSlotNumber);
     }
 
-    public static void main(String[] args) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, SignatureException, InvalidKeyException {
-        Provider pkcs11Provider = securityProvider.getInstance();
-
-        char[] pin = "1234".toCharArray();
-        KeyStore keyStore;
-
-        keyStore = KeyStore.getInstance("PKCS11", pkcs11Provider);
-        keyStore.load(null, pin);
-
-        KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA", pkcs11Provider);
-        gen.initialize(2048);
-        KeyPair keyPair = gen.generateKeyPair();
-
+    private static X509Certificate generateCertificate(KeyPair keyPair) throws CertificateEncodingException, NoSuchAlgorithmException, SignatureException, InvalidKeyException {
         X509V3CertificateGenerator certificateGenerator = new X509V3CertificateGenerator();
         certificateGenerator.setSerialNumber(BigInteger.valueOf(1));   //or generate a random number
         certificateGenerator.setSubjectDN(new X509Principal("CN=localhost"));  //see examples to add O,OU etc
@@ -57,13 +44,30 @@ public class RSASigning {
         certificateGenerator.setNotAfter(Date.from(afterDate.toInstant()));
         certificateGenerator.setSignatureAlgorithm("SHA256withRSA");
 
-        PrivateKey privateKey = keyPair.getPrivate();
-        X509Certificate certificate = certificateGenerator.generate(privateKey);
+        return certificateGenerator.generate(keyPair.getPrivate());
+    }
 
-        keyStore.setKeyEntry("key1", keyPair.getPrivate(), "1234".toCharArray(), new X509Certificate[]{certificate});
+    public static void main(String[] args) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, SignatureException, InvalidKeyException {
+        Provider pkcs11Provider = securityProvider.getInstance();
 
-        certificate = (X509Certificate) keyStore.getCertificate("key1");
-        privateKey = (PrivateKey) keyStore.getKey("key1", "1234".toCharArray());
+        char[] pin = "1234".toCharArray();
+        KeyStore keyStore;
+
+        keyStore = KeyStore.getInstance("PKCS11", pkcs11Provider);
+        keyStore.load(null, pin);
+
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey("key1", "1234".toCharArray());
+        X509Certificate certificate = (X509Certificate) keyStore.getCertificate("key1");
+
+        if (privateKey == null && certificate == null) {
+            KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA", pkcs11Provider);
+            gen.initialize(2048);
+            KeyPair keyPair = gen.generateKeyPair();
+
+            privateKey = keyPair.getPrivate();
+            certificate = generateCertificate(keyPair);
+            keyStore.setKeyEntry("key1", privateKey, "1234".toCharArray(), new X509Certificate[]{certificate});
+        }
 
         System.out.println("Enter the text to be sign: ");
         Scanner s = new Scanner(System.in);
@@ -87,9 +91,6 @@ public class RSASigning {
         boolean isValid = singingObjVerify.verify(digitalSignature);
 
         String isValidResponse = isValid ? "valid" : "invalid";
-        System.out.println("The signature is "+ isValidResponse);
-
-        System.out.println(singingObj);
-
+        System.out.println("The signature is " + isValidResponse);
     }
 }
